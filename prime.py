48d8c9b34654682d9a30276c790035c5a9f4f63b
pavankalyan-c <45252372+pavankalyan-c@users.noreply.github.com>
2018-11-28 12:45:00 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+while true:
+	number1=int(input("Enter Number"))
+	number2=number1+1
+	multipliers=list()
+	for j in range(number1,o,-1)
+	multipliers.append(j)
+	#print(j)
+	import operator
+	import functools
+	result=functools.reduce(operate.mul,multipliers,1)
+	print(result)
